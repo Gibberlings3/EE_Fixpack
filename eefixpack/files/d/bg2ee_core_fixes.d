b07c3661663fbf78590d8bf64a02f054df8352fe
CamDawg <pcamagna@yahoo.com>
2022-03-27 14:56:57 -0700

    Seeding BG2EE with some misc fixes

---
+++ eefixpack/files/d/bg2ee_core_fixes.d
@@ -0,0 +1,3 @@
+// tbd, cam (two issues here, see yoshimo.bcs)
+// Yoshimo Should Have Follow-Up Banters with the PC
+ADD_TRANS_ACTION byoshim BEGIN 11 END BEGIN END ~SetGlobalTimer("YoshimoTalksPC2","GLOBAL",FIVE_DAYS)~ // add timer to prompt second banter
