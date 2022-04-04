91e54cd8c4691279c97cf0c3bec6096e4a23698a
CamDawg <pcamagna@yahoo.com>
2022-04-04 16:39:35 -0700

    Some basic BGEE fixes

---
+++ eefixpack/files/d/bgee_core_fixes.d
@@ -0,0 +1,9 @@
+// tbd, cam
+// broken DV check for bassilus
+REPLACE_TRIGGER_TEXT ~ftowbe~  ~Dead("bassil")~ ~Dead("bassilus")~ 
+REPLACE_TRIGGER_TEXT ~mtowbe~  ~Dead("bassil")~ ~Dead("bassilus")~ 
+REPLACE_TRIGGER_TEXT ~mtowbez~ ~Dead("bassil")~ ~Dead("bassilus")~ 
+
+// tbd, cam
+// broken DV check for bassilus
+REPLACE_TRIGGER_TEXT ~ftowbe~  ~Dead("FLAMHUSB")~ ~Dead("FlamingE")~ 
