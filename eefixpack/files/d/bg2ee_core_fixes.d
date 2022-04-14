2b591e15d88b6e2f38eb5e46681c97a30a8ea1fd
CamDawg <pcamagna@yahoo.com>
2022-04-14 13:07:10 -0700

    More fixes, mainly bg2ee.tph
    https://www.gibberlings3.net/forums/topic/35203-bg2ee-fixes-from-bg2fp/

--- eefixpack/files/d/bg2ee_core_fixes.d
+++ eefixpack/files/d/bg2ee_core_fixes.d
@@ -1,3 +1,7 @@
 // tbd, cam (two issues here, see yoshimo.bcs)
 // Yoshimo Should Have Follow-Up Banters with the PC
 ADD_TRANS_ACTION byoshim BEGIN 11 END BEGIN END ~SetGlobalTimer("YoshimoTalksPC2","GLOBAL",FIVE_DAYS)~ // add timer to prompt second banter
+
+// tbd, cam
+// ranger stronghold fails if you're in Umar Hills when Delon spawn timer expires; see also ar1100.bcs
+ADD_TRANS_ACTION delon BEGIN 21 END BEGIN END ~SetGlobal("CDDelonSpoke","GLOBAL",1)~
