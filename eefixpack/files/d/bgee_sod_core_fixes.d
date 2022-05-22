47b998bb719106d5a3e61822a8f30cdc58472308
CamDawg <pcamagna@yahoo.com>
2022-05-22 10:55:55 -0700

    Another fix for the SoD den of thieves quest
    https://www.gibberlings3.net/forums/topic/35005-bug-sod-issues-from-jastey/page/2/#comment-309766

--- eefixpack/files/d/bgee_sod_core_fixes.d
+++ eefixpack/files/d/bgee_sod_core_fixes.d
@@ -3,3 +3,9 @@
 EXTEND_BOTTOM bdstoneh 54
   IF ~!Global("bd_sdd305_den_of_thieves","global",0)~ THEN DO ~AddJournalEntry(61530,QUEST)~ EXIT
 END  
+
+// tbd, cam
+// threatening Waizahb as a paladin doesn't spawn Rhynwis, stalling the quest
+ADD_TRANS_ACTION BDWAIZAH BEGIN 3 END BEGIN 0 END ~SaveLocation("LOCALS","bd_default_loc",[2337.300])
+SetGlobal("bd_retreat","locals",1)
+CreateCreature("bdrhynwi",[2246.246],SW)~
