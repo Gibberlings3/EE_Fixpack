be3988db523c5edda53aea84075b475510146df9
CamDawg <pcamagna@yahoo.com>
2022-07-27 13:42:19 -0700

    [sod] Bugbears vs. shadow, rewards from Kharm
    Kharm only gives rewards in one of his four replies:
    https://www.gibberlings3.net/forums/topic/35005-bug-sod-issues-from-jastey/page/2/#comment-311498
    
    Don't tell Shadow to attack bugbears if they're already dead:
    https://www.gibberlings3.net/forums/topic/35005-bug-sod-issues-from-jastey/page/3/#comment-311500

--- eefixpack/files/d/bgee_sod_core_fixes.d
+++ eefixpack/files/d/bgee_sod_core_fixes.d
@@ -18,3 +18,11 @@ ADD_TRANS_TRIGGER bdhelvda  3 ~!Dead("BDHELVDA") !Dead("BDSIMONE") !Global("SDD3
 ADD_TRANS_TRIGGER bdmizhen 27 ~!Dead("BDHELVDA") !Dead("BDSIMONE") !Global("SDD303_ACCUSED_COL","GLOBAL",1) !Global("sdd303_seal_found","bd3000",2)~ 38 77 78 DO 0
 ADD_TRANS_TRIGGER bdmizhen 27 ~!Dead("BDHELVDA") !Dead("BDSIMONE") !Global("SDD303_ACCUSED_COL","GLOBAL",1) !Global("sdd303_seal_found","bd3000",2)~ 38       DO 1
 ADD_TRANS_TRIGGER bdsimone 1  ~!Dead("BDHELVDA") !Dead("BDSIMONE") !Global("SDD303_ACCUSED_COL","GLOBAL",1) !Global("sdd303_seal_found","bd3000",2)~ 2        DO 0
+
+// tbd, cam
+// kharm only gives out reward on one dialogue branch
+ADD_TRANS_ACTION bdkharm BEGIN 1 END BEGIN 1 2 3 END ~GiveGoldForce(250)~
+
+// tbd, cam
+// don't tell shadow to kill bugbears if they're already dead
+ADD_TRANS_TRIGGER bdshadow  2 ~Global("cdBugbearsDead","LOCALS",0)~ DO 2 3 4
