28022c6f2baa72445e6b9932a3a1f7fca9950c93
CamDawg <pcamagna@yahoo.com>
2022-10-31 10:46:53 -0700

    [bg] Mulahey stutter fix
    https://www.gibberlings3.net/forums/topic/35178-scripting-bugs/#comment-308786

--- eefixpack/files/d/bgee_core_fixes.d
+++ eefixpack/files/d/bgee_core_fixes.d
@@ -8,6 +8,17 @@ REPLACE_TRIGGER_TEXT ~mtowbez~ ~Dead("bassil")~ ~Dead("bassilus")~
 // broken DV check for bassilus
 REPLACE_TRIGGER_TEXT ~ftowbe~  ~Dead("FLAMHUSB")~ ~Dead("FlamingE")~ 
 
+// tbd, cam
+// mulahey stutter fix (see also mulahey.bcs) https://www.gibberlings3.net/forums/topic/35178-scripting-bugs/#comment-308786
+ADD_STATE_TRIGGER mulahe 3 ~Global("TalkedToMulahey","GLOBAL",0)~  
+SET_WEIGHT mulahe 7 #0 
+SET_WEIGHT mulahe 3 #1 
+SET_WEIGHT mulahe 0 #2 
+SET_WEIGHT mulahe 1 #3 
+SET_WEIGHT mulahe 5 #4 
+SET_WEIGHT mulahe 6 #5 
+SET_WEIGHT mulahe 8 #6
+
 //tbd, cam
 // tiax's broken charm dialogue, NVLOR for x2 talk
 ALTER_TRANS tiax BEGIN 8 END BEGIN 0 END BEGIN ~REPLY~ ~~ END // blank dialogue text prevents exiting
