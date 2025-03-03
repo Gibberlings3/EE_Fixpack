1e7385a5b77a462e2c366d9d5491b98f141edd16
CamDawg <pcamagna@yahoo.com>
2025-03-03 07:13:38 -0800

    [bg2] consolidating bernard's stores
    https://www.gibberlings3.net/forums/topic/34875-bug-report-ee-content-bugs/page/11/#findComment-318324

--- eefixpack/files/d/bg2ee_core_fixes.d
+++ eefixpack/files/d/bg2ee_core_fixes.d
@@ -73,3 +73,6 @@ ALTER_TRANS trax BEGIN 15 END BEGIN 1 END BEGIN ~REPLY~ ~#522~ END // All right,
 // 'nothing to say to you' bug in hexxat's quest
 ALTER_TRANS OHHSTONE BEGIN 1 END BEGIN 2 END ACTION ~~ END
 ALTER_TRANS OHHSTONE BEGIN 2 END BEGIN 0 END EPILOGUE ~EXIT~ END
+
+// combine bernard's store (see also bernard.bcs, bernard2.sto)
+REPLACE_ACTION_TEXT ~bernard~ ~StartStore("bernard"~ ~StartStore("bernard2"~
