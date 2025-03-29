79df75ca5d7c8df0a58d798d9e79ab636c1ee7d2
Argent77 <4519923+Argent77@users.noreply.github.com>
2025-03-29 14:16:05 +0100

    [pst] Fix incorrect number of charges in various item creation script actions
    https://www.gibberlings3.net/forums/topic/39361-pstee-some-items-are-created-with-an-incorrect-number-of-charges

--- eefixpack/files/d/pstee_core_fixes.d
+++ eefixpack/files/d/pstee_core_fixes.d
@@ -35,3 +35,21 @@ REPLACE_TRANS_ACTION DMARISSA BEGIN 49 END BEGIN 0 END ~UseItem("M_Gaze",Protago
 // Nihl Xander should give you an existing 'Pacify' scroll when following the Dreambuilder conversation branch
 REPLACE_TRANS_ACTION DXANDER BEGIN 22 END BEGIN 1 END ~GiveItemCreate("PacScrll",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("SPWI108",Protagonist,1,1,0)~
 REPLACE_TRANS_ACTION DXANDER BEGIN 25 END BEGIN 0 1 END ~GiveItemCreate("PacScrll",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("SPWI108",Protagonist,1,1,0)~
+
+// Items should be created with the right number of charges
+REPLACE_TRANS_ACTION DCANDRI BEGIN 36 END BEGIN 0 END ~GiveItemCreate("NToken",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("NToken",Protagonist,17,0,0)~
+REPLACE_TRANS_ACTION DIANNIS BEGIN 29 30 104 END BEGIN 0 END ~GiveItemCreate("Dlegacy2",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Dlegacy2",Protagonist,1,0,0)~
+REPLACE_TRANS_ACTION DIANNIS BEGIN 29 30 104 END BEGIN 0 END ~GiveItemCreate("HearChrm",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("HearChrm",Protagonist,1,0,0)~
+REPLACE_TRANS_ACTION DIANNIS BEGIN 70 END BEGIN 0 END ~GiveItemCreate("Maslow",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Maslow",Protagonist,33,0,0)~
+
+// Spell scrolls should be created with the right number of charges
+REPLACE_TRANS_ACTION DGHIVERM BEGIN 47 END BEGIN 0 1 END ~GiveItemCreate("SPWI107",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("SPWI107",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DKITLA BEGIN 37 END BEGIN 0 1 END ~GiveItemCreate(\("[^"]+"\),Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate(\1,Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DDAKKON BEGIN 294 END BEGIN 2 8 10 END ~GiveItemCreate(\("[^"]+"\),Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate(\1,Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DDAKKON BEGIN 299 END BEGIN 0 END ~GiveItemCreate("Circle03",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Circle03",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DDAKKON BEGIN 305 END BEGIN 0 END ~GiveItemCreate("Circle04",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Circle04",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DDAKKON BEGIN 320 END BEGIN 0 END ~GiveItemCreate("Circle07",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Circle07",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DDAKKON BEGIN 324 END BEGIN 0 END ~GiveItemCreate("Circle08",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Circle08",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DEBB BEGIN 65 74 END BEGIN 0 END ~GiveItemCreate("SPWI505",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("SPWI505",Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DIGNUS BEGIN 67 71 76 80 END BEGIN 0 END ~GiveItemCreate(\("[^"]+"\),Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate(\1,Protagonist,1,1,0)~
+REPLACE_TRANS_ACTION DNORDOM BEGIN 84 END BEGIN 0 END ~GiveItemCreate("Enoll",Protagonist,[0-9]+,[0-9]+,[0-9]+)~ ~GiveItemCreate("Enoll",Protagonist,1,1,0)~
