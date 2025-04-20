1da3051637f9098612bf1e25f2a5fb0374c1958b
CamDawg <pcamagna@yahoo.com>
2025-04-19 20:28:19 -0700

    [pst] all branches leading to Black-Barbed Seed sprouting should consume the seed
    https://www.gibberlings3.net/forums/topic/39467-pstee-black-barbed-seed-should-not-remain-in-inventory-after-talking-to-mourns-for-trees/

--- eefixpack/files/d/pstee_core_fixes.d
+++ eefixpack/files/d/pstee_core_fixes.d
@@ -60,6 +60,9 @@ ALTER_TRANS damarys BEGIN 4 END BEGIN 0 1 END BEGIN ~TRIGGER~ ~GlobalLT("Nodd_Qu
 ALTER_TRANS damarys BEGIN 5 END BEGIN 0 3 END BEGIN ~TRIGGER~ ~GlobalLT("Nodd_Quest","GLOBAL",3)~ END // excluded 1
 ALTER_TRANS damarys BEGIN 7 END BEGIN 0 2 END BEGIN ~TRIGGER~ ~GlobalLT("Nodd_Quest","GLOBAL",3)~ END // excluded 1
 
+// all branches leading to Black-Barbed Seed sprouting should consume the seed
+ADD_TRANS_ACTION dmftree BEGIN 56 57 END BEGIN 2 END ~DestroyPartyItem("BSeed1",TRUE)~
+
 /////                                                  \\\\\
 ///// mixing instants and non-instants                 \\\\\
 /////                                                  \\\\\
